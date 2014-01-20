0acd985da3b3116e3173d525520760a1607520bb
preston@nooch.com <preston@nooch.com>
2014-01-20 17:05:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Realeaf::Application.config.secret_key_base = 'd088dd3a3420f3a879116a8546d032d83f2f4d283f805d6e3e2e0a0d3c0a67c5bdc4d187f8a9c02e065ee3aa4afeeb2d434c8847be4d33eb4bc13d598dc15375'
