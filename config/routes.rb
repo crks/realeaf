eb168b940deb1fb915378a019491497530964748
preston@nooch.com <preston@nooch.com>
2014-01-20 19:03:29 -0500

    Static home/help pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Realeaf::Application.routes.draw do
+  get "home/index"
+  get "home/help"
   resources :messages
 
   resources :profiles
@@ -9,7 +11,7 @@ Realeaf::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
